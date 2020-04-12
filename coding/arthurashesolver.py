6579de2f59867924242b20b6d9f7fb4bf5811588
pleoxconfusa <cbonyadi@gmail.com>
2020-04-12 18:31:16 -0400

    Update arthurashesolver.py

--- coding/arthurashesolver.py
+++ coding/arthurashesolver.py
@@ -2,6 +2,8 @@
 """
 Created on Mon Mar 16 14:46:02 2020
 
+"Success is a journey, not a destination. The doing is often more important than the outcome." - Arthur Ashe
+
 @author: pleoxconfusa
 """
 
